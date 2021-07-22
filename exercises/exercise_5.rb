a3c04037d8a4428f2be6dd1fcacf47f8dea8a875
Vivian Cheung <hi@viviancheung.ca>
2021-07-22 18:34:19 +0000

    calculations

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,13 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+sum = Store.sum(:annual_revenue)
+
+puts "Sum: #{sum}"
+
+average = sum / Store.count
+
+puts "Average annual revenue: #{average}"
+
+puts Store.where("annual_revenue < ?", 1000000 ).count
+
