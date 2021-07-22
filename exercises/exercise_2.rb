3a8422608665857423869b47d45be76ea3545efa
Vivian Cheung <hi@viviancheung.ca>
2021-07-22 16:46:51 +0000

    update the name of first store

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,14 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+# Your code goes heregit sta ...
+puts "store 1"
+@store1 = Store.find(1)
+puts @store1.name
+puts "store 2"
+@store2 = Store.find(2)
+puts @store2.name
+puts "rename store 1"
+@store1.name = "Haritzia"
+@store1.save
+puts @store1.name
