e2f63ff5196033f4e6fb8014807816c20813764e
Vivian Cheung <hi@viviancheung.ca>
2021-07-22 18:12:57 +0000

    loading subset of stores

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,16 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+store = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+store = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+store = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+puts Store.all.count
+
+@mens_stores = Store.where(mens_apparel: true)
+@mens_stores.each { |store| puts "store name: #{store.name} annual revenue: #{store.annual_revenue}"}
+
+@womens_stores = Store.where("womens_apparel = ? AND annual_revenue < ?", true, 1000000)
+@womens_stores.each{ |store| puts "store name: #{store.name} annual revenue: #{store.annual_revenue}" }
+
+
